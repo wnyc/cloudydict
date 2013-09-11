5aa6957316543cf552f31f27868a8a0cdedc1219
Adam DePrince <deprince@googlealumni.com>
2013-09-11 12:23:51 -0400

    #1 Version 0.0.10

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup
 
 setup(
     name='cloudydict',
-    version='0.0.9',
+    version='0.0.10',
     author='Adam DePrince',
     author_email='adeprince@nypublicradio.org',
     description='Dictionary interface to cloud providers.',
